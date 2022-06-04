c08e495c5a8c110f1d883e9821fd5dda8025627f
Priyanuj Kashyap <72681447+priyanujkashyap@users.noreply.github.com>
2022-06-04 20:59:16 +0530

    Delete File extention reader.py

--- File extention reader.py
+++
@@ -1,9 +0,0 @@
-Extns= {"py":"Python","c":"C","cpp":"C++","jar":"Java"}
-filename = input("Input the Filename: ")
-f_extns = filename.split(".")
-if f_extns[1] in Extns.keys():
-    print("The extention of the file is:", Extns.get(f_extns[1]))
-else:
-    print("No data available")
-
-
